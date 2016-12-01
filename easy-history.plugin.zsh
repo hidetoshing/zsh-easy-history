0204190f73133545aca6aba96e007fea2cd4a3ec
hidetoshing <hidetoshing@gmail.com>
2016-12-01 21:02:46 +0900

    :art: add plugin file

---
+++ easy-history.plugin.zsh
@@ -0,0 +1,28 @@
+
+# historical backward/forward search with linehead string binded to ^P/^N
+autoload history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^p" history-beginning-search-backward-end
+bindkey "^n" history-beginning-search-forward-end
+bindkey "\\ep" history-beginning-search-backward-end
+bindkey "\\en" history-beginning-search-forward-end
+
+# Command history configuration
+setopt hist_ignore_dups     # ignore duplication command history list
+setopt HIST_IGNORE_SPACE
+setopt share_history        # share command history data
+setopt inc_append_history
+
+HISTFILE=${HOME}/.zsh_history
+HISTSIZE=100000
+SAVEHIST=$HISTSIZE
+
+function history-select() {
+    BUFFER=`history -n 1 | fzy`
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+
+zle -N history-select
+bindkey "^@" history-select
